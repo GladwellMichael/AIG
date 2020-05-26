abb3888ec63dd8b7e62f733512b78955056cd737
moyowashe1997 <54856006+moyowashe1997@users.noreply.github.com>
2020-05-26 12:21:32 +0000

    after modofying function for categorising data

--- AIG Assignment 1/regularisedlogisticregression.jl
+++ AIG Assignment 1/regularisedlogisticregression.jl
@@ -1,33 +1,31 @@
-using CSV
+using CSV ,DataFrames
 using Statistics
-using  PyCall
-using PrettyTables
 
 
 #reading dataset for experimenting purpose
 # df =CSV.read("ex2data1.csv")
 da =CSV.read("bank-additional-full.csv")
 
-
+#fanction for cleaning dataset
 function  datacleaning(da)
+
 #removing duplicates
 dat = unique(da)
 
 #removing some of the column
-delete!(dat,:age)
-delete!(dat,:contact)
-delete!(dat,:day_of_week)
-delete!(dat,:month)
-delete!(dat,:marital)
+    select!(dat,Not(:age))
+    select!(dat,Not(:contact))
+    select!(dat,Not(:day_of_week))
+    select!(dat,Not(:month))
+    select!(dat,Not(:marital))
 return dat
 end
 
+#envoking  datacleaning method
 dat = datacleaning(da)
 
 #converting dataframe to matrix
-# df = convert(Matrix,df)
 dat = convert(Matrix,dat)
-
 
 
 # function for standadising the x train
@@ -47,116 +45,37 @@ function transform_features(x_test ,mean , standard_deviation)
 end
 
 
-
-
-
-#function for cleaning data
-############################################
+#function for categorising data
 function categorisingData(data)
 
-#getting the length of the data set
-x = size(data)[1]
+     for i = 1:(size(data)[2])
+        if(i == size(data)[2])
+            for j = 1:size(data)[1]
+                if(data[j, i] == "yes")
+                    data[j, i] = Int(1)
+                else
+                    data[j, i] = Int(0)
+                end
+            end
+        else
+            identical = unique(data[:,i])
+            for j = 1:size(data)[1]
+                for k = 1:size(identical)[1]
+                        if(data[j,i] == identical[k])
+                            data[j,i] = Int(k)
+                        end
+                end
+            end
+        end
+    end
 
-#looping over the  dataset
-for i in 1:x
-    
-# cotegorising the job title column
-if(data[i,1]=="housemaid")
-    data[i,1]=1
-elseif(data[i,1]=="services")
-    data[i,1]=2
-elseif(data[i,1]=="admin.")
-    data[i,1]=3
-elseif(data[i,1]=="blue-collar")
-    data[i,1]=4
-elseif(data[i,1]=="technician")
-    data[i,1]= 5
-elseif(data[i,1]=="retired")
-    data[i,1]=6
-elseif(data[i,1]=="management")
-    data[i,1]=7
-elseif(data[i,1]=="unemployed")
-    data[i,1]=8
-elseif(data[i,1]=="entrepreneur")
-    data[i,1]=9
-elseif(data[i,1]=="self-employed")
-    data[i,1]=10
-elseif(data[i,1]=="student")
-    data[i,1]=11
-elseif(data[i,1]=="unknown")
-    data[i,1]=0
-                                        
+#convertiing from array of any to array of float64
+d = convert(Array{Float64},data[:,:])
+    return d
 end
-
-
-
-#categorising  education  status
-if(data[i,2]=="unknown")
-    data[i,2]=0
-elseif(data[i,2]=="high.school")
-    data[i,2]=1
-elseif(data[i,2]=="basic.6y")
-    data[i,2]=2
-elseif(data[i,2]=="basic.9y")
-    data[i,2]=3
-elseif(data[i,2]=="professional.course")
-    data[i,2]=4
-elseif(data[i,2]=="basic.4y")
-    data[i,2]=5
-elseif(data[i,2]=="university.degree")
-    data[i,2]=6
-end
-
-#categorising default  column
-if(data[i,3]=="no")
-    data[i,3]=1
-elseif(data[i,3]=="unknown")
-    data[i,3]=2
-end
-
-#categorising housing column
-if(data[i,4]=="yes")
-    data[i,4]=1
-elseif(data[i,4]=="no")
-    data[i,4]=2
-elseif(data[i,4]=="unknown")
-    data[i,4]= 0
-end
-
-#categorising loan column
-if(data[i,5]=="yes")
-    data[i,5]=1
-elseif(data[i,5]=="no")
-    data[i,5]=2
-elseif(data[i,5]=="unknown")
-    data[i,5]=0
-end
-
-
-#categorising poutcome column
-if(data[i,10]=="nonexistent")
-    data[i,10]=1
-elseif(data[i,10]=="failure")
-    data[i,10]=2
-elseif(data[i,10]=="success")
-    data[i,10]=3
-end
-
-# categorising the label (0 and 1)
-if(data[i,16]=="yes")
-    data[i,16]=1
-elseif(data[i,16]=="no")
-    data[i,16]=0
-end
-
-end
-    return data
-end
-############################################
 
 #calling the function for categorising data
 df = categorisingData(dat)
-
 
 # data for experimenting
 #splting data into  features(X) and labels(y)
@@ -185,72 +104,63 @@ function sigmoid(z)
     return 1 /(1+ exp(-z[1]))
 end
 
-# hypothsesis function for calculating predicting y
+#function for calculating predicting y
 function hypothesis(z)
     return sigmoid(z)
 end
 
 
 # cost function for computing the cost of all taining sample
-function regularised_cost_function(features,y,theta,lambda)
-    m ,n= size(features)
-    estimate = hypothesis(features*theta)
-
+function regularised_cost_function(estimate,y,theta,lambda)
+    m = length(y)
     total_cost = ((-y)' * log.(estimate))-((1 .- y)' * log.(1 .- estimate)) .+  (lambda/(2*m) *  broadcast(abs, theta))
-    
-    # derivative = (1/m) * (X') * (estimate.-y) + ((1/m) * (lambda * theta))
-    # print("Derivative",derivative)
-    derivative =  (1/m) * (features[:, 1])' * (estimate.-y).+  (lambda/(2*m) *  broadcast(abs, theta))
-    # print("Derivative ",derivative)
-    return  ( total_cost, derivative)
+    return  total_cost
 end
 
 
-function fit(X,y,lambda,learning_rate,number_of_iterations= 800)
+#fuction for calculating the derivative 
+function derivative(estimate, y, X)
+       derivative_of_cost = (estimate - y)*X
+       return derivative_of_cost
+end
+
+
+#function for calculating  gradient descend
+function gradient_descent(X,y,lambda,learning_rate)
     length=size(X)[2]
+    #initialising theta with zeros
+    theta = zeros(Int, 1, size(X, 2))
     
-    # print(length)
-    theta = zeros(length)  
-   
-    cost= zeros(number_of_iterations,2950)
-    for i in 1:number_of_iterations
-        cost[i,:],gradient = regularised_cost_function(X[i,:],y,theta,lambda)
-        theta = theta .- (learning_rate * gradient)
-        
+    #calculating gradient and updating theta
+    for i in 1:(size(X)[1])
+        gradient = derivative(hypothesis(theta*X[i, :]), y[i], X[i, :] )
+        theta = theta .- (learning_rate * gradient) .+ lambda* broadcast(abs, theta)
     end
-    # print("Theta :",cost)
-    return (theta,cost)
+    
+    return theta
 end
 
-theta,cost  = fit(X_train,y_train,0.001,10)
 
-
-# #function for making prediction on unseen adata
-# ############################################
+# #function for making prediction on unseen data
 function predict(xx)
-    theta,cost  = fit(X_train,y_train,0.001,10)
-    print(size(theta))
-    print(size(xx))
+    #getting the updated theta from gradient descent
+    theta  = gradient_descent(X_train,y_train,0.001,10)
+
+    #intialising the probability array to hold the calculated estimate
     probability = zeros(size(xx)[1])
     
+    #multiplying the X-test sample with updated theta
     for i in 1:(size(xx)[1])
-        result = transpose(xx[i, :]) .* theta
-        print("Result :",result)
-        probability[i]= sigmoid(result)
+        probability[i]= hypothesis(theta*X[i, :])
     end
-    
+
     return probability
 end
-############################################
-
-prediction = predict(X_test)
-print("Predictions :",prediction)
 
 
 # #confusion matrix (accurace,pecision,recall)
-# ############################################
 function confusion_matrix(estimate , actual)
-
+    #initialising the variable for using to calculate CM
     positive_class = Int64[]
     negative_class = Int64[]
     true_positive = 0
@@ -264,6 +174,7 @@ function confusion_matrix(estimate , actual)
 
 
     for i in 1:(length(actual))
+
         #splitting positive and negative elments
         if(actual[i]==1.0)
             push!(positive_class,actual[i])
@@ -271,10 +182,9 @@ function confusion_matrix(estimate , actual)
             push!(negative_class,actual[i])
         end
 
-       
-        if(estimate[i]==1.0 )
+       #getting TP and FN
+        if(estimate[i]== 0 )
             if(actual[i]== estimate[i])
-            print("\n",i)
             true_positive = true_positive +1 
             elseif(actual[i] != estimate[i])
             false_negative = false_negative +1
@@ -282,9 +192,8 @@ function confusion_matrix(estimate , actual)
         end
 
         #getting TN and FP
-        if(estimate[i]== 0 )
+        if(estimate[i]== 1 )
             if(actual[i]== estimate[i])
-            print("\n",i)
             true_negative = true_negative +1 
             elseif(actual[i] != estimate[i])
             false_positive = false_positive +1
@@ -301,10 +210,16 @@ function confusion_matrix(estimate , actual)
     #returning a tuple
     return (accurace ,recall ,precision)
 end
-############################################
 
+
+#envoking the methoin predict 
+prediction = predict(X_test)
+
+
+#envoking the method confusion matrix
 accurace ,recall ,precision = confusion_matrix(prediction,y_test)
 
 print("\nrecall :",recall)
 print("\naccurace :",accurace)
 print("\nprecision  :",precision)
+print("\n")
